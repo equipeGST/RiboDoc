4ff434944cd802ee755f271a743bfa77e3396a43
HugoAi2bc <hugo.arbes@i2bc.paris-saclay.fr>
2021-10-04 10:01:16 +0200

    add onerror runle and handle similar file names for mapping outputs

--- RiboDoc/RiboDoc.sh
+++ RiboDoc/RiboDoc.sh
@@ -1,5 +1,5 @@
 ################################################################################
-#########################           RiboDoc           ################e#############
+#######################           RiboDoc           ############################
 ################################################################################
 
 ################################################################################
@@ -18,7 +18,6 @@ cpu_use=$((${cpu}/4));
 echo "Minimum CPU used for the analysis : "${cpu_use};
 
 # conda list;
-snakemake -s /RiboDoc/RiboDoc/Snakefile -j --dag -np |  dot -Tsvg > /data/dag_last-run.svg;
-snakemake -s /RiboDoc/RiboDoc/Snakefile -j --dag -np --forceall |  dot -Tsvg > /data/dag_all.svg;
-# snakemake -s /RiboDoc/RiboDoc/Snakefile -j ${cpu_use};
+snakemake -s /RiboDoc/RiboDoc/Snakefile -j --dag -np | dot -Tsvg > /data/dag_last-run.svg;
+snakemake -s /RiboDoc/RiboDoc/Snakefile -j --dag -np --forceall | dot -Tsvg > /data/dag_all.svg;
 snakemake -s /RiboDoc/RiboDoc/Snakefile -j ${cpu_use} -k;
