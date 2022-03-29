b14c9ab361421d7459a3b138426e24bd334af3ef
equipeGST <gmt.seq@gmail.com>
2022-03-29 18:04:55 +0200

    Handling CPU and RAM capacity provided by the user

--- RiboDoc/RiboDoc.sh
+++ RiboDoc/RiboDoc.sh
@@ -9,21 +9,24 @@
 
 # If the user does not provide a minimum number of cpu available :
 # a quarter of computer resources is assigned for the analysis (doubled on specific steps)
+used_memory=""
 if [ $# -eq 0 ];
 then
     cpu=`nproc --all`;
     echo "Total CPU available : "${cpu};
     cpu_use=$((${cpu}/4));
+    available_memory_mb=""
 else
     cpu_use=$(($1/2));
+    available_memory_mb=$(($2*1000))
+    echo "Maximum RAM used for the analysis : ${available_memory_mb}Mb";
 fi;
 echo "Maximum CPU used for the analysis : "$((${cpu_use}*2));
 
-available_memory_kb=`grep MemTotal /proc/meminfo | sed -E "s/^[^0-9]+([0-9]+)[^0-9]+$/\1/"`;
-available_memory_mb=$((${available_memory_kb}/1000))
-used_memory=$((${available_memory_mb}/4*3))
+used_memory="--resources mem_mb=${available_memory_mb}"
 
 # conda list;
 snakemake -s /work/RiboDoc/Snakefile -j --dag -np | dot -Tsvg > /data/dag_last-run.svg;
 snakemake -s /work/RiboDoc/Snakefile -j --dag -np --forceall | dot -Tsvg > /data/dag_all.svg;
-snakemake -s /work/RiboDoc/Snakefile -j ${cpu_use} -k --resources mem_mb=${used_memory};
+snakemake -s /work/RiboDoc/Snakefile -j ${cpu_use} -k ${used_memory};
+# snakemake -s /ORFribo_RiboDoc/RiboDoc_BAM2Reads/Snakefile -j ${cpu_use} -k --resources mem_mb=${used_memory};
