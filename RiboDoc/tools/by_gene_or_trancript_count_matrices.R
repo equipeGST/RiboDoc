2e17e744270b59e29379904e5c05f28145a5b78f
equipeGST <gmt.seq@gmail.com>
2023-03-24 15:56:47 +0100

    Split the step of matrices construction by gene or by transcript

---
+++ RiboDoc/tools/by_gene_or_trancript_count_matrices.R
@@ -0,0 +1,26 @@
+# Path to project directory
+local_path <- "/data/"
+
+# Read the config file
+params <- scan(file = paste0(local_path, "config.yaml"),
+               what = "character",
+               sep = ":"
+)
+
+# Load designed R functions
+source(paste0(local_path, "tools/DESeq2_analysis_functions.R"))
+
+# Path to count tables folder
+paths_list <- DESeq2_folder_paths(local_path)
+
+# Make counts rounded for future differential analyses
+expData = round(read.table(paths_list$pathway_matrix, header = T, row.names = 1, check.names = FALSE, sep="\t"))
+
+# Load names corresponding to transcript IDs
+names_list = read.table(paths_list$pathway_names, header = T, row.names = 1, check.names = FALSE, sep="\t")
+
+# Create count matrices by gene of by transcript
+expData_transcript <- transcript_or_gene(data = expData, list_of_names = names_list)
+
+# Write the ID of the most expressed transcript for each gene
+most_expressed_transcript(data = expData_transcript, list_of_names = names_list)
