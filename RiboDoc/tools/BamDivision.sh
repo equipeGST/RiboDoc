11c3ed3598b568caa4b3a0ac27f1e5da2dfe3598
equipeGST <gmt.seq@gmail.com>
2023-04-11 17:43:20 +0200

    Quote variables and file names

--- RiboDoc/tools/BamDivision.sh
+++ RiboDoc/tools/BamDivision.sh
@@ -37,25 +37,22 @@ if [ -z "${B}" ] || [ -z "${l}" ] || [ -z "${N}" ] ; then
 fi
 #if threads empty, default value = 4
 if [ -z "${T}" ]; then
-	T=4
+	T=4;
 fi
 #if output directory is empty, write in working directory
 if [ -z "${o}" ]; then
-	o="."
+	o=".";
 fi
-
-# Output directory
-mkdir -p ${o}"bamDivision"
 
 #Bam Division
 # Division of reads depending of read lengths
-gawk -v l=${l} '{if($1 !~ /^ *@/){if(length($10)==l){print $0}}else{print $0}}' $(samtools view -@ ${T} -h ${B}) > ${o}bamDivision/${N}.${l}.sam;
+gawk -v l="${l}" '{if($1 !~ /^ *@/){if(length($10)==l){print $0}}else{print $0}}' <<< $(samtools view -@ ${T} -h ${B}) > ${o}bamDivision/${N}.${l}.sam;
 
 #Conversion as bam
-samtools view -@ ${T} -b ${o}bamDivision/${N}.${l}.sam > ${o}bamDivision/${N}.${l}.uniq.bam ;
-rm -rf ${o}bamDivision/${N}.${l}.sam
+samtools view -@ "${T}" -b "${o}bamDivision/${N}.${l}.sam" > "${o}bamDivision/${N}.${l}.uniq.bam";
+rm -rf "${o}bamDivision/${N}.${l}.sam";
 # Sort bam file
-samtools sort -@ ${T} ${o}bamDivision/${N}.${l}.uniq.bam -o ${o}bamDivision/${N}.${l}.uniq.sort.bam ;
-rm -rf ${o}bamDivision/${N}.${l}.uniq.bam
+samtools sort -@ "${T}" "${o}bamDivision/${N}.${l}.uniq.bam" -o "${o}bamDivision/${N}.${l}.uniq.sort.bam";
+rm -rf "${o}bamDivision/${N}.${l}.uniq.bam";
 # Index bam file
-samtools index ${o}bamDivision/${N}.${l}.uniq.sort.bam;
+samtools index "${o}bamDivision/${N}.${l}.uniq.sort.bam";
