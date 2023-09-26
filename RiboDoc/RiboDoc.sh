1714418b14a80b4dccf3cde83b2dbf885fb79e37
equipeGST <gmt.seq@gmail.com>
2023-09-26 16:22:33 +0200

    Add a 'cd' to '/data/' folder before launching Snakemake

--- RiboDoc/RiboDoc.sh
+++ RiboDoc/RiboDoc.sh
@@ -8,7 +8,8 @@
 ################################################################################
 
 if [ $# -eq 0 ]|| [ $# -eq 1 ]; then
-    echo "PLEASE SPECIFY AVAILABLE RESOURCES BY ADDING CPUs AND MEMORY AMOUNT (in GB)"
+    printf "PLEASE SPECIFY AVAILABLE RESOURCES BY ADDING CPUs AND MEMORY AMOUNT (in GB)\n"
+    printf "Example (with 10 CPU and 30GB of memory) :\n\tsingularity run --bind /path/to/workdir/ ribodoc_v0.9.0.19.sif 10 30\n"
 else
     if [ "$1" -eq "1" ]; then
         cpu_use=$(($1/2))
@@ -16,12 +17,13 @@ else
         cpu_use=$1
     fi
     available_memory_mb=$(($2*1000))
-    echo "Maximum RAM used for the analysis : ${available_memory_mb}Mb"
-    echo "Maximum CPU used for the analysis : "$((${cpu_use}*2))
+    printf "Maximum RAM used for the analysis : %sMb\n" "${available_memory_mb}"
+    printf "Maximum CPU used for the analysis : %s\n" $((${cpu_use}*2))
 
     used_memory="--resources mem_mb=${available_memory_mb}"
 
-    # conda list;
+    # To be sure the hidden '.snakemake/' folder is in the working directory, snakemake must be invoqued in '/data/'
+    cd /data/
     snakemake -s /RiboDoc/RiboDoc/Snakefile --rerun-incomplete -j --dag -np --forceall | dot -Tsvg > /data/dag_all.svg
     snakemake -s /RiboDoc/RiboDoc/Snakefile -k --rerun-incomplete -j ${cpu_use}
 fi
