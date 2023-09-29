002e7ced45965b29575c593d00e03744279c872a
equipeGST <gmt.seq@gmail.com>
2023-09-29 16:17:43 +0200

    Differential analysis : improve MA-plot and volcano-plots display and add most significant gene names

--- RiboDoc/tools/DESeq2_analysis_functions.R
+++ RiboDoc/tools/DESeq2_analysis_functions.R
@@ -285,87 +285,144 @@ padj_distribution <- function(deseq_results) {
        main = "Histogram adjusted p-values")
 }
 
-# MA-plot
-ma_plot <- function(deseq_results) {
-  deseq_results_modified <- deseq_results
-  deseq_results_modified$padj[which(abs(deseq_results_modified$log2FoldChange) <= abs(Var_log2FC))] <-
-    1
-  plotMA(
-    deseq_results_modified,
-    alpha = Var_padj,
-    ylab = "log2FC",
-    colSig = rgb(1, 0, 0, 0.5)
-  )
+ma_plot <- function(deseq_results, data_list) { # MA-plot function
+  data = data.frame(deseq_results)
+  fdr = Var_padj
+  fc = Var_log2FC
+  
+  detection_call = rep(1, nrow(data))
+  
+  data$baseMean <- log2(data$baseMean +1)
+  genenames <- rownames(data)
+  
+  sig <- rep(3, nrow(data))
+  sig[which(data$padj <= fdr & data$log2FoldChange < 0 & abs(data$log2FoldChange) >= log2(fc) & detection_call ==1)] = 2
+  sig[which(data$padj <= fdr & data$log2FoldChange > 0 & abs(data$log2FoldChange) >= log2(fc) & detection_call ==1)] = 1
+  data <- data.frame(name = genenames, mean = data$baseMean, lfc = data$log2FoldChange, padj = data$padj, sig = sig)
+  
+  . <- NULL
+  data$sig <- as.factor(data$sig)
+  .lev <- levels(data$sig) %>% as.numeric()
+  new.levels <- c(paste0("Up: ", sum(sig == 1)), paste0("Down: ", sum(sig == 2)), "NS") %>% .[.lev]
+  
+  data$sig <- factor(data$sig, labels = new.levels)
+  
+  data <- data[order(data$padj), ]
+  
+  complete_data <- stats::na.omit(data)
+  labs_data <- subset(complete_data, padj <= fdr & name!="" & abs(lfc) >= log2(fc))
+  labs_data <- utils::head(labs_data, 5)
+  
+  # Plot
+  mean <- lfc <- sig <- name <- padj <-  NULL
+  
+  ma_plot <- ggplot(data, aes(x = mean, y = lfc)) +
+    geom_point(aes(alpha = sig, colour=sig, fill=sig, size=sig), shape=21) +
+    scale_colour_manual(values = c("red", "red", "grey")) +
+    scale_size_manual(values= c(1, 1, 0.5), guide = guide_legend(override.aes = list(size = 5))) +
+    scale_alpha_manual(values = c(1, 1, 0.5)) +
+    scale_fill_manual(values = c("red", "red", "grey")) +
+    ggrepel::geom_label_repel(data = labs_data, mapping = aes(label = name),
+                              box.padding = unit(0.35, "lines"),
+                              point.padding = unit(0.3, "lines"),
+                              force = 1, seed = 42, fontface = "plain",
+                              size = 12/3, color = "black",
+                              max.overlaps = Inf,
+                              show.lgend = FALSE) + 
+    scale_x_continuous(breaks=seq(0, max(data$mean), 2)) +
+    labs(x = "Mean of normalized counts",
+         y = "log2FC", 
+         title = paste0("MA-plot - ",data_list$Cond[data_list$nb_col_condB+1]," vs ", data_list$Cond[1]), 
+         color = "Expression change")+ # to remove legend title use color = ""
+    geom_hline(yintercept = c(0, -log2(fc), log2(fc)), linetype = c(1, 2, 2),
+               color = c("black", "black", "black")) + 
+    guides(
+      color = FALSE,  # Remove the legend for color
+      alpha = FALSE,  # Remove the legend for alpha
+      size = FALSE
+    ) + 
+    theme_classic() +
+    theme(legend.position="bottom",
+          plot.title = element_text(face = "bold", hjust=0.5, size=20),
+          panel.border = element_rect(colour = "black", fill = NA, size= 0.5),    
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank(),
+          axis.text=element_text(size=12),
+          axis.title=element_text(size=14,face="bold"),
+          legend.title = element_blank(),
+          legend.text=element_text(size=12))
+  
+  return(ma_plot)
 }
 
 # Volcano plot global
-volcano_plot <- function(deseq_results, data_list) {
-  myColors <-
-    ifelse((
-      deseq_results$padj < Var_padj &
-        deseq_results$log2FoldChange > Var_log2FC
-    ),
-    rgb(1, 0, 0, 0.25) ,
-    ifelse((
-      deseq_results$padj < Var_padj &
-        deseq_results$log2FoldChange < -Var_log2FC
-    ),
-    rgb(1, 0, 0, 0.25) ,
-    rgb(0, 0, 0, 0.25)
-    )
+volcano_plot <- function(deseq_results, data_list) { # Volcano plot function
+  deseq_results_modified <- data.frame(deseq_results) # Create a new dataframe with the results of DESeq2
+  deseq_results_modified$diffexpressed <- ifelse( # Add a new column with the expression change
+    abs(deseq_results_modified$log2FoldChange) > Var_log2FC & deseq_results_modified$padj < Var_padj,
+    "Significant", # If the gene is differentially expressed
+    "NS") # If the gene is not differentially expressed
+  
+  limit_x <- max(abs(ceiling(range(deseq_results_modified$log2FoldChange[!is.na(deseq_results_modified$log2FoldChange)])))) # Set the limit of the x-axis
+  limit_y <- -log10(range(deseq_results_modified$padj[!is.na(deseq_results_modified$padj)])) # Set the limit of the y-axis
+  
+  top_significant_values <- deseq_results_modified %>% filter(padj %in% sort(deseq_results_modified$padj)[1:5]) # Select the 5 most significant values
+  
+  up_genes <- nrow(subset(deseq_results_modified, log2FoldChange > Var_log2FC & padj < Var_padj)) # Count the number of up-regulated genes
+  down_genes <- nrow(subset(deseq_results_modified, log2FoldChange < Var_log2FC & padj < Var_padj)) # Count the number of down-regulated genes
+  
+  volcano_plot_global <- ggplot(deseq_results_modified, aes(x = log2FoldChange, y = -log10(padj))) + # Create the volcano plot object
+    geom_point(aes(colour = diffexpressed, alpha = diffexpressed, size = diffexpressed), shape = 16) + # Add the values
+    geom_point(data = top_significant_values, aes(colour = "Significant"), shape = 21, size = 3, fill = "red", colour = "black") + # Add the 5 most significant values
+    geom_hline(yintercept = -log10(Var_padj), linetype = "dashed") + # Horizontal line at p-value = 0.05
+    geom_vline(xintercept = c(-Var_log2FC, Var_log2FC), linetype = "dashed") + # Vertical line at log2FC = 1
+    geom_label_repel(
+      data = top_significant_values, # Add labels for the 5 most significant values
+      aes(x = log2FoldChange, y = -log10(padj), label = row.names(top_significant_values)),
+      force = 2,
+      nudge_y = 1
+    ) +
+    scale_colour_manual(values = c("Significant" = "red", "NS" = "grey"), name =NULL) + 
+    scale_alpha_manual(values = c("Significant" = 1, "NS" = 0.5), name =NULL) +
+    scale_size_manual(values = c("Significant" = 2, "NS" = 1), name =NULL) +
+    scale_fill_manual(values = c("Significant" = "red", "NS"="grey"), name="Expression change") + # If you want the fill color for significant points
+    scale_x_continuous(breaks = c(seq(-limit_x, limit_x, 5)), limits = c(-limit_x, limit_x)) + # Set the limits of the x-axis 
+    guides(
+      color = guide_legend(override.aes = list(size = 5)) # Remove the legend for the alpha aesthetic
+    ) +
+    labs(
+      title = paste0("Volcano plot - ", data_list$Cond[data_list$nb_col_condB + 1], " vs ", data_list$Cond[1]),
+      x = "log2(fold change)",
+      y = "-log10(adjusted P-value)",
+      colour = "Expression change"
+    ) +
+    geom_text(x = Inf, y = Inf, label = paste("Up:", up_genes, "\n Down:", down_genes), hjust = 1, vjust = 1.5, size = 4, color = "black") +
+    theme_bw() +
+    theme(
+      legend.position = "bottom",
+      plot.title = element_text(face = "bold", hjust = 0.5, size = 20),
+      panel.border = element_rect(colour = "black", fill = NA, size = 0.5),    
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank(),
+      axis.text = element_text(size = 12),
+      axis.title = element_text(size = 14, face = "bold"),
+      legend.title = element_blank(),
+      legend.text = element_text(size = 12)
     )
   
-  plot(
-    deseq_results[, "log2FoldChange"],
-    -log10(deseq_results[, "padj"]),
-    pch = 20,
-    cex = 1,
-    col = myColors,
-    xlab = "log2 FC",
-    ylab = "-log10(padj)",
-    main = paste0("Volcano plot - ", data_list$Cond[data_list$nb_col_condB +
-                                                      1], " vs ", data_list$Cond[1])
-  )
+  return(volcano_plot_global)
 }
 
-# ZOOM Volcano plot
+# Zoomed Volcano plot function
 volcano_plot_zoom <- function(deseq_results, data_list) {
-  limite <- Var_padj / (10 ^ (-log10(Var_padj) * 2))
+  limite <- Var_padj/(10^(-log10(Var_padj)*2)) # Set the limit of the y-axis
   
+  zoomed_in_volcano_plot <- volcano_plot(deseq_results, data_list) + # Create the zoomed volcano plot object
+    scale_y_continuous(limits = c(0, -log10(limite))) + # Set the limits of the y-axis
+    geom_point(data = data.frame(subset(data.frame(deseq_results), padj < limite)), aes(y=-log10(limite)), colour="red", alpha = 0.5, shape = 17, size = 3) +
+    labs(title = paste0("Volcano plot Zoom - ",data_list$Cond[data_list$nb_col_condB+1]," vs ",data_list$Cond[1]))
   
-  myColors <-
-    ifelse((
-      deseq_results$padj < Var_padj &
-        deseq_results$log2FoldChange > Var_log2FC
-    ),
-    rgb(1, 0, 0, 0.25) ,
-    ifelse((
-      deseq_results$padj < Var_padj &
-        deseq_results$log2FoldChange < -Var_log2FC
-    ),
-    rgb(1, 0, 0, 0.25) ,
-    rgb(0, 0, 0, 0.25)
-    )
-    )
-  
-  Valeurs_Limite_Y <- ifelse((deseq_results$padj < limite), limite ,
-                             deseq_results$padj)
-  
-  Forme_Pixel_Y <- ifelse((deseq_results$padj < limite), 17 ,
-                          20)
-  
-  
-  plot(
-    deseq_results[, "log2FoldChange"],
-    -log10(Valeurs_Limite_Y),
-    pch = Forme_Pixel_Y,
-    col = myColors,
-    xlab = "log2 FC",
-    ylab = "-log10(padj)",
-    ylim = c(0, -log10(limite)),
-    main = paste0("Volcano plot Zoom - ", data_list$Cond[data_list$nb_col_condB +
-                                                           1], " vs ", data_list$Cond[1])
-  )
+  return(zoomed_in_volcano_plot)
 }
 
 # Final tables creation
