ef456a3c3d3ba0db11779447b5925e77a28f44ee
enoracrl <enora.corler@gmail.com>
2025-05-07 17:03:40 +0200

    add check samplesheet script

--- RiboDoc/tools/others/check_samplesheet.py
+++ RiboDoc/tools/others/check_samplesheet.py
@@ -4,18 +4,34 @@ import os
 import sys
 import errno
 import argparse
+from pathlib import Path
 
-def parse_args(args=None):
+def parse_args(args=None) -> argparse.Namespace:
+    """
+    Parse command line arguments.
+    
+    Args:
+        args (list) : List of arguments to parse.
+        
+    Returns:
+        argparse.Namespace : Object containing parsed arguments.
+    """
     Description = "Reformat input samplesheet file and check its contents."
-    Epilog = "Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"
+    Epilog      = "Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
-    parser.add_argument("FILE_IN", help="Input samplesheet file.")
-    parser.add_argument("UPDATED_PATH", help="Input path for test_nobc_nodx_rnamod")
-    parser.add_argument("FILE_OUT", help="Output file.")
+    parser.add_argument(
+        "FILE_IN", 
+        help = "Input samplesheet file.")
+    parser.add_argument(
+        "FILE_OUT",
+        help = "Output file.")
     return parser.parse_args(args)
 
 def make_dir(path):
+    """
+    Make a directory if it doesn't exist.
+    """
     if len(path) > 0:
         try:
             os.makedirs(path)
@@ -23,8 +39,17 @@ def make_dir(path):
             if exception.errno != errno.EEXIST:
                 raise exception
 
-def read_head(handle, num_lines=10):
-    """Read the specified number of lines from the current position in the file."""
+def read_head(handle, num_lines:int=10) -> str:
+    """
+    Read the specified number of lines from the current position in the file.
+    
+    Args:
+        handle (str)   : File handle.
+        num_lines (int) : Number of lines to read.
+        
+    Returns:
+        str : Lines read from the file.
+    """
     lines = []
     for idx, line in enumerate(handle):
         if idx == num_lines:
@@ -33,7 +58,13 @@ def read_head(handle, num_lines=10):
     return "".join(lines)
 
 def print_error(error, context:str="Line", context_str:str=""):
-    """Print an error based on context and end script"""
+    """
+    Print an error based on context and end script
+    
+    Args:
+        error (str)       : Error message.
+        context (str)     : Context of error.
+    """
     error_str = "ERROR: Please check samplesheet -> {}".format(error)
     if context != "" and context_str != "":
         error_str = "ERROR: Please check samplesheet -> {}\n{}: '{}'".format(
@@ -42,21 +73,24 @@ def print_error(error, context:str="Line", context_str:str=""):
     print(error_str)
     sys.exit(1)
 
-def check_samplesheet(file_in:str, updated_path:SyntaxError, file_out:str):
+def check_samplesheet(file_in:str, file_out:str) -> None:
     """
     This function checks that the samplesheet follows the following structure:
-    sample	replicate	fastq1	fastq2	strandedness	barcode	adapter	fasta	gff
-    Mutant	1	Mutant.1.fastq.gz		forward		AGATCGGAAGAGCACACGTCTGAACTCCAGTCA	genome.fa	genome.gff
-    Mutant	2	Mutant.1.fastq.gz		forward		AGATCGGAAGAGCACACGTCTGAACTCCAGTCA	genome.fa	genome.gff
-    WT	1	WT.1.fastq.gz		forward		AGATCGGAAGAGCACACGTCTGAACTCCAGTCA	genome.fa	genome.gff
-    WT	2	WT.2.fastq.gz		forward		AGATCGGAAGAGCACACGTCTGAACTCCAGTCA	genome.fa	genome.gff
+    
+    sample	replicate	type	fastq1	fastq2	strandedness	barcode	adapter	fasta	gff
+    Mutant	1	riboseq	Mutant.1.fastq.gz		forward		AGATCGGAAGAGCACACGTCTGAACTCCAGTCA	genome.fa	genome.gff
+    Args:
+        file_in (str)  :        Input samplesheet file.
+        file_out (str) :        Output samplesheet file.
+    Returns:
+        None
     """
     input_extensions = []
     sample_info_dict = {}
     with open(file_in, "r") as f:
         ## Check header
         min_cols = 3
-        header = ["sample", "replicate", "fastq1", "fastq2", "strandedness", "barcode", "adapter",
+        header = ["condition", "replicate", "type", "fastq1", "fastq2", "strandedness", "barcode", "adapter1", "adapter2",
                   "fasta", "gff"]
         line = f.readline().strip().split("\t")
         if line[: len(header)] != header:
@@ -77,7 +111,7 @@ def check_samplesheet(file_in:str, updated_path:SyntaxError, file_out:str):
                 print_error("Invalid number of populated columns (minimum = {})!".format(min_cols), "Line", line)
 
             ## Check group name entries
-            group, replicate, barcode, input_file, fasta, gtf = lspl[: len(header)]
+            group, replicate, type, fastq1, fastq2, strandedness, barcode, adapter1, adapter2, fasta, gtf = lspl[: len(header)]
             if group:
                 if group.find(" ") != -1:
                     print_error("Group entry contains spaces!", "Line", line)
@@ -92,6 +126,48 @@ def check_samplesheet(file_in:str, updated_path:SyntaxError, file_out:str):
                 print_error("Replicate id not specified!", "Line", line)
             replicate = int(replicate)
 
+            ## Check type name entries
+            if type:
+                if type.find(" ") != -1:
+                    print_error("Type entry contains spaces!", "Line", line)
+                if type not in ["riboseq", "rnaseq"]:
+                    print_error("Type entry must be 'riboseq' or 'rnaseq'!", "Line",
+                                line)
+            else:
+                print_error("Type entry not specified!", "Line", line)
+
+            # Check input file(s)
+            if fastq1:
+                if fastq2:
+                    input_file = [fastq1, fastq2]
+                else:
+                    input_file = [fastq1]
+                
+                for fastq in input_file:
+                    file = Path(fastq)
+                    if not file.is_file():
+                        
+                        print_error("One of your input file does not exist!", "Line", line)
+                    if fastq.find(" ") != -1:
+                        print_error("One of your input FASTQ file contains spaces!", "Line", line)
+                    if fastq.endswith(".fastq.gz"):
+                        input_extensions.append("*.fastq.gz")
+                    elif fastq.endswith(".fq.gz"):
+                        input_extensions.append("*.fq.gz")
+                    else:
+                        print_error("Path does not end with '.fastq.gz' or '.fq.gz' extension!", "Line", line)
+            else:
+                print_error("Fastq1 entry not specified!", "Line", line)
+
+            ## Check strandedness entry
+            if strandedness:
+                if strandedness.find(" ") != -1:
+                    print_error("Strandedness entry contains spaces!", "Line", line)
+                if strandedness not in ["forward", "reverse", "auto"]:
+                    print_error("Strandedness entry should be either forward, reverse or auto!", "Line", line)
+            else:
+                print_error("Strandedness entry not specified!", "Line", line)
+
             ## Check barcode entry
             if barcode:
                 if not barcode.isdigit():
@@ -99,53 +175,26 @@ def check_samplesheet(file_in:str, updated_path:SyntaxError, file_out:str):
                 else:
                     barcode = "barcode%s" % (barcode.zfill(2))
 
-            ## Check input file extension
-            nanopolish_fast5 = ""
-            if input_file:
-                if input_file.find(" ") != -1:
-                    print_error("Input file contains spaces!", "Line", line)
-                if input_file.endswith(".fastq.gz"):
-                    input_extensions.append("*.fastq.gz")
-                elif input_file.endswith(".fq.gz"):
-                    input_extensions.append("*.fq.gz")
-                elif input_file.endswith(".bam"):
-                    input_extensions.append("*.bam")
-                else:
-                    if updated_path != "not_changed":
-                        input_file = "/".join([updated_path, input_file.split("/")[-1]])
-                    list_dir = os.listdir(input_file)
-                    nanopolish_fast5 = input_file
-                    if not all(fname.endswith(".fast5") for fname in list_dir):
-                        if "fast5" in list_dir and "fastq" in list_dir:
-                            nanopolish_fast5 = input_file + "/fast5"
-                            ## CHECK FAST5 DIRECTORY
-                            if not all(fname.endswith(".fast5") for fname in os.listdir(nanopolish_fast5)):
-                                print_error("fast5 directory contains non-fast5 files.")
-                            ## CHECK PROVIDED BASECALLED FASTQ
-                            fastq_path = input_file + "/fastq"
-                            basecalled_fastq = [
-                                fn for fn in os.listdir(fastq_path) if fn.endswith(".fastq.gz") or fn.endswith(".fq.gz")
-                            ]
-                            if len(basecalled_fastq) != 1:
-                                print_error("Please input one basecalled fastq per sample.")
-                            else:
-                                input_file = fastq_path + "/" + basecalled_fastq[0]
-                                if not basecalled_fastq[0].endswith(".fastq.gz"):
-                                    if not basecalled_fastq[0].endswith(".fq.gz"):
-                                        print_error('basecalled fastq input does not end with ".fastq.gz" or ".fq.gz"')
-                        else:
-                            print_error(
-                                'path does not end with ".fastq.gz", ".fq.gz", or ".bam" and is not an existing directory with correct fast5 and/or fastq inputs.'
-                            )
+            ## Check adapter entry
+            if adapter1:
+                if adapter1.find(" ") != -1:
+                    print_error("Adapter entry contains spaces!", "Line", line)
+                if not all(x in ["A", "T", "C", "G"] for x in adapter1):
+                    print_error("Adapter entry contains others characters than A, T, C or G ! ", "Line", line)
+            else:
+                print_error("adapter entry not specified!", "Line", line)
 
             ## Check genome entries
             if fasta:
+                file = Path(fasta)
+                if not file.is_file():
+                    print_error("Fasta file does not exist!", "Line", line)
                 if fasta.find(" ") != -1:
                     print_error("Genome entry contains spaces!", "Line", line)
                 if len(fasta.split(".")) > 1:
                     if (
                         fasta[-6:] != ".fasta"
-                        and fasta[-6:] != ".fasta"
+                        and fasta[-3:] != ".fa"
                         and fasta[-4:] != ".fna"
                         and fasta[-7:] != ".fna.gz"
                         and fasta[-9:] != ".fasta.gz"
@@ -156,32 +205,32 @@ def check_samplesheet(file_in:str, updated_path:SyntaxError, file_out:str):
                             "Line",
                             line,
                         )
-
+                
             ## Check transcriptome entries
-            # gtf = ''
             is_transcripts = "0"
             if gtf:
+                file = Path(gtf)
+                if not file.is_file():
+                    print_error("GFF file does not exist!", "Line", line)
                 if gtf.find(" ") != -1:
-                    print_error("Transcriptome entry contains spaces!", "Line", line)
-                print(gtf[-4:])
-                if gtf[-4:] != ".gtf" and gtf[-7:] != ".gtf.gz":
-                    print_error("Transcriptome entry does not have extension '.gtf' or '.gtf.gz'!", "Line", line)
-                # if transcriptome[-6:] != '.fasta' and transcriptome[-3:] != '.fa' and transcriptome[-9:] != '.fasta.gz' and transcriptome[-6:] != '.fa.gz' and transcriptome[-4:] != '.gtf' and transcriptome[-7:] != '.gtf.gz':
-                #    print_error("Transcriptome entry does not have extension '.fasta', '.fa', '.fasta.gz', '.fa.gz', '.gtf' or '.gtf.gz'!", 'Line', line)
-                # if transcriptome[-4:] == '.gtf' or transcriptome[-7:] == '.gtf.gz':
-                #    gtf = transcriptome
-                #    if not genome:
-                #        print_error("If genome isn't provided, transcriptome must be in fasta format for mapping!", 'Line', line)
-                # else:
-                #    is_transcripts = '1'
-                #    genome = transcriptome
+                    print_error("Genes annotation entry contains spaces!", "Line", line)
+                if (
+                    gtf[-4:] != ".gtf"
+                    and gtf[-7:] != ".gtf.gz"
+                    and gtf[-5:] != ".gff3"
+                    and gtf[-8:] != ".gff3.gz"
+                    and gtf[-4:] != ".gff"
+                    and gtf[-7:] != ".gff.gz"
+                ):
+                    print_error("Genes annotation entry does not have extension '.gtf', 'gff', 'gtf.gz' or '.gff.gz'!", "Line", line)
 
             ## Create sample mapping dictionary = {group: {replicate : [ barcode, input_file, genome, gtf, is_transcripts, nanopolish_fast5 ]}}
-            sample_info = [barcode, input_file, fasta, gtf, is_transcripts, nanopolish_fast5]
-            if group not in sample_info_dict:
-                sample_info_dict[group] = {}
-            if replicate not in sample_info_dict[group]:
-                sample_info_dict[group][replicate] = sample_info
+            sample_info = [fastq1, fastq2, barcode, adapter1, adapter2, fasta, gtf, is_transcripts]
+            
+            if f"{group}_{type}" not in sample_info_dict:
+                sample_info_dict[f"{group}_{type}"] = {}
+            if replicate not in sample_info_dict[f"{group}_{type}"]:
+                sample_info_dict[f"{group}_{type}"][replicate] = sample_info
             else:
                 print_error("Same replicate id provided multiple times!", "Line", line)
 
@@ -199,7 +248,7 @@ def check_samplesheet(file_in:str, updated_path:SyntaxError, file_out:str):
         make_dir(out_dir)
         with open(file_out, "w") as fout:
             fout.write(
-                ",".join(["sample", "barcode", "input_file", "fasta", "gtf", "is_transcripts", "nanopolish_fast5"])
+                "\t".join(["sample", "fastq1", "fastq2", "barcode", "adapter1", "adapter2", "fasta", "gtf", "is_transcripts"])
                 + "\n"
             )
             for sample in sorted(sample_info_dict.keys()):
@@ -210,13 +259,18 @@ def check_samplesheet(file_in:str, updated_path:SyntaxError, file_out:str):
 
                 ### Write to file
                 for replicate in sorted(sample_info_dict[sample].keys()):
-                    sample_id = "{}_R{}".format(sample, replicate)
-                    fout.write(",".join([sample_id] + sample_info_dict[sample][replicate]) + "\n")
+                    sample_id = "{}.{}".format(sample, replicate)
+                    fout.write("\t".join([sample_id] + sample_info_dict[sample][replicate]) + "\n")
 
 def main(args=None):
-    """Run main function"""
+    """
+    Run main function
+    
+    Args:
+        args (list) : List of arguments to parse.
+    """
     args = parse_args(args)
-    check_samplesheet(args.FILE_IN, args.UPDATED_PATH, args.FILE_OUT)
+    check_samplesheet(args.FILE_IN, args.FILE_OUT)
 
 if __name__ == "__main__":
     sys.exit(main())
